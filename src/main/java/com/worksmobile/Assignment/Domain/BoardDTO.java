3269ec50b28dd05caddf48f048933e9b23c05a8f
ryu <dkstntnfy@naver.com>
2018-03-26 17:23:03 +0900

    파일 업로드 다운로드 기능 추가

--- src/main/java/com/worksmobile/Assignment/Domain/BoardDTO.java
+++ src/main/java/com/worksmobile/Assignment/Domain/BoardDTO.java
@@ -1,19 +1,40 @@
 package com.worksmobile.Assignment.Domain;
 
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public class BoardDTO {
-	private int id;
+	private int board_id;
 	private String subject;
 	private String content;
 	private String created;
-	private MultipartFile attachment;
-	public int getId() {
-		return id;
+	
+	private byte[] attachment;
+	private String fileName;
+	private long fileSize;
+	
+	public String getFileName() {
+		return fileName;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public long getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(long l) {
+		this.fileSize = l;
+	}
+	
+	public int getBoard_id() {
+		return board_id;
+	}
+
+	public void setBoard_id(int board_id) {
+		this.board_id = board_id;
 	}
 	public String getSubject() {
 		return subject;
@@ -33,10 +54,10 @@ public class BoardDTO {
 	public void setCreated(String created) {
 		this.created = created;
 	}
-	public MultipartFile getAttachment() {
+	public byte[] getAttachment() {
 		return attachment;
 	}
-	public void setAttachment(MultipartFile attachment) {
+	public void setAttachment(byte[] attachment) {
 		this.attachment = attachment;
 	}
 
