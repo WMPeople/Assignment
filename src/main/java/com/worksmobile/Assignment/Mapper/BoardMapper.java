a353b97e7a0380b1f2f43c66663adf5bf176a507
ryu <dkstntnfy@naver.com>
2018-03-26 17:23:37 +0900

    파일 업로드 다운로드 기능 추가

--- src/main/java/com/worksmobile/Assignment/Mapper/BoardMapper.java
+++ src/main/java/com/worksmobile/Assignment/Mapper/BoardMapper.java
@@ -10,8 +10,13 @@ public interface BoardMapper {
 
 	public int boardCreate(BoardDTO board) throws Exception;
 
-	public int boardDelete(int id) throws Exception;
+	public int boardDelete(int board_id) throws Exception;
 
 	public int boardUpdate(BoardDTO board) throws Exception;
+	
+	public BoardDTO viewDetail(int board_it) throws Exception;
+	
+	public BoardDTO boardFileDownload(int board_it) throws Exception;
+	
 
 }
