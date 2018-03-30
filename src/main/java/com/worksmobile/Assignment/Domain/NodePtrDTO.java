8e9005b6cf1d817f1ca316fa7ee35704da9f105a
ryu <dkstntnfy@naver.com>
2018-03-30 15:25:24 +0900

    커밋 실수 해결

--- src/main/java/com/worksmobile/Assignment/Domain/NodePtrDTO.java
+++ src/main/java/com/worksmobile/Assignment/Domain/NodePtrDTO.java
@@ -1,20 +1,30 @@
-	@Override
-	public boolean equals(Object arg0) {
-		NodePtrDTO dto = (NodePtrDTO)arg0;
-		return	board_id == dto.board_id &&
-				version == dto.version &&
-				branch == dto.branch;
+package com.worksmobile.Assignment.Domain;
+
+import java.util.HashMap;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class NodePtrDTO {
+	@Getter @Setter protected Integer board_id = null;
+	@Getter @Setter protected Integer version = null;
+	@Getter @Setter protected Integer branch = null;
+	
+	public NodePtrDTO() { }
+	public NodePtrDTO(Integer board_id, Integer version, Integer branch) {
+		this.board_id = board_id;
+		this.version = version;
+		this.branch = branch;
+	}
+
+	public HashMap<String, Integer> toMap() {
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("board_id", board_id);
+		map.put("version", version);
+		map.put("branch", branch);
+		return map;
 	}
 	
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("board_id : ").append(board_id)
-				.append("version : ").append(version)
-				.append("branch : ").append(branch);
-		return builder.toString();
-	}
-}
 	private static boolean checkEquals(Integer lhs, Integer rhs) {
 		return lhs == null ? (rhs == null ? true : false) : lhs.equals(rhs);
 	}
