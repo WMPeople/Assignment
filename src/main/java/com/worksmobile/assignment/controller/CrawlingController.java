4048bbed2f42a9054fab7562365bf36af6f3241b
KimHongHun <yumezi7@gmail.com>
2018-05-01 10:32:02 +0900

    예외시 404에러가 뜰 수 있도록 수정 이어서
    - 트리 그려주는 기능에서 404 예외를 떤지도록 변경.
    - 트리 그려주는 컨트롤러에서 예외처리를 하지 않도록 수정.
      404에러 뜰 수 있도록
    - 자동저장 테스트 컴파일 에러 수정.(master 브렌치 코드 가져왔습니다.)

--- src/main/java/com/worksmobile/assignment/controller/CrawlingController.java
+++ src/main/java/com/worksmobile/assignment/controller/CrawlingController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.worksmobile.assignment.bo.BrowserCrawlingService;
@@ -18,7 +19,7 @@ import com.worksmobile.assignment.bo.NaverAPIService;
  * @author rws
  *
  */
-@org.springframework.web.bind.annotation.RestController
+@RestController
 public class CrawlingController {
 
 	@Autowired
