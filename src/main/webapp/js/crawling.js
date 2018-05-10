b22762820bb9260209a57818db9d91c47efe152d
ryu <dkstntnfy@naver.com>
2018-05-10 17:06:04 +0900

    1. 영화 '엑스맨'  , 쇼핑 '선풍기' 형식의 문구를 인식 하도록 구현. 2. if - else if문 switch case문으로 변경 3. dialog next page , previous 페이지 생성

--- src/main/webapp/js/crawling.js
+++ src/main/webapp/js/crawling.js
@@ -35,64 +35,121 @@ var crawling_text;
 pageNo = 1;
 
 $(function() {
-	var lines = $('#content3').val().split("\n");
-	var firstLine = lines[0];
-	var textList = firstLine.split(' ');
+	// textarea 내용 복사
+	var copyContent = $('#content3').val();
+	var keywordList = [];
+	var nameList = ["위치", "영화", "책", "도서" , "맛집", "뉴스", "쇼핑", "음식점", "단어" , "영어단어", "구매", "지도"];
 	
-	if (textList[0] == '책' || textList[0] == '도서') {
-		category = 'book';
-		for(var i = 1 ; i < textList.length ; i ++) {
-			crawling_text = textList[i];
-		}
-	} else if (textList[0] == '영화') {
-		category = 'movie';
-		for(var i = 1 ; i < textList.length ; i ++) {
-			crawling_text = textList[i];
-		}
-	} else if (textList[0] == '뉴스') {
-		category = 'news';
-		for(var i = 1 ; i < textList.length ; i ++) {
-			crawling_text = textList[i];
-		}
-	} else if (textList[0] == '쇼핑' || textList[0] == '구매') {
-		category = 'shop';
-		for(var i = 1 ; i < textList.length ; i ++) {
-			crawling_text = textList[i];
-		}
-	} //kakao api
-	else if (textList[0] == '지도' || textList[0] == '위치') {
-		category = 'geocode';
-		for(var i = 1 ; i < textList.length ; i ++) {
-			crawling_text = textList[i];
-		}
-	} //네이버 크롤링
-	else if (textList[0] == '단어' || textList[0] == '영어단어') {
-		category = 'dictionary';
-		for(var i = 1 ; i < textList.length ; i ++) {
-			crawling_text = textList[i];
-		}
-	} //네이버 크롤링
-	else if (textList[0] == '맛집' || textList[0] == '음식점') {
-		category = 'local';
-		for(var i = 1 ; i < textList.length ; i ++) {
-			crawling_text = textList[i];
+	//keywordList 리스트에  ["영화" , "그린팩토리"] 형식으로 푸쉬된다.
+	for(var i =0; i< nameList.length ; i++){
+		var searchIndex = 0;
+		while(searchIndex != -1){
+			var searchIndex = copyContent.search(nameList[i]);
+			var curIndexSpace = '';
+			for(var j =0 ; j< nameList[i].length ; j++){
+				tempSpace += ' ';
+			}
+			copyContent = copyContent.replace(nameList[i],tempSpace);
+			if(searchIndex!= -1){
+				var spaceIndex = searchIndex + nameList[i].length;
+				var firstQuoteIndex = spaceIndex + 1;
+				if(copyContent.charAt(spaceIndex) == ' ' && copyContent.charAt(firstQuoteIndex) == "'"){
+					var startIndex = firstQuoteIndex + 1;
+					var secondQuoteIndex = copyContent.substring(startIndex).search("'") + startIndex + 1;
+					var text = copyContent.substring(firstQuoteIndex + 1,secondQuoteIndex - 1);
+					copyContent = copyContent.substring(0,searchIndex) + copyContent.substring(secondQuoteIndex);
+					if (text == "") {
+						continue;
+					} else {
+						var list = [nameList[i] , text]
+						keywordList.push(list);		
+					}
+					
+				}
+			}	
 		}
 	}
-
-	var url = getUrl(category, encodeURI(crawling_text), 1);
-	dialogFunction(category, url);
+	
+	//배열 중복 제거, 자원 감소
+	function unique(array) {
+		  var tempArray = [];
+		  var resultArray = [];
+		  for(var i = 0; i < array.length; i++) {
+		    var item = array[i]
+		    if(tempArray.includes(item[0])) {
+		      continue;
+		    } else {
+		      resultArray.push(item);
+		      tempArray.push(item[0]);
+		    }
+		  }
+		  return resultArray;
+		}
+	keywordList = unique(keywordList);
+	var url_category_text = [];
+	
+	//name 별로 다른 url을 호출한다.
+	for(var i = 0 ; i < keywordList.length ; i++){
+		switch(keywordList[i][0]) {
+		case '책': category = 'book';
+		case '도서':
+			break;
+			
+		case '영화':
+			category = 'movie';
+			break;
+			
+		case '뉴스':
+			category = 'news';
+			break;
+			
+		case '쇼핑':
+		case '구매':
+			category = 'shop';
+			break;
+			
+		case '지도':
+		case '위치':
+			category = 'geocode';
+			break;
+			
+		case '단어':
+		case '영어단어':
+			category = 'dictionary';
+			break;
+			
+		case '맛집':
+		case '음식점':
+			category = 'local';
+			break;
+			
+		default:
+			break;
+		}
+		crawling_text = keywordList[i][1];
+		console.log(crawling_text);
+		url = getUrl(category, encodeURI(crawling_text), 1);
+		var list = [url , category, crawling_text];
+		url_category_text.push(list);
+		
+	}
+	dialogFunction(url_category_text);
 });
 
-function dialogFunction(crawling_category, url) {
+function dialogFunction(url_category_text) {
+	var curIndex = 0;
 	$("#dialog").dialog({
 				open : function() {
-					$(this).load(url);
-					doWhenDialogLoad(this, category, crawling_text);
+					$(this).load(url_category_text[0][0]);
+					doWhenDialogLoad(this, url_category_text[0][1], url_category_text[0][2]);
+					 $("#dialog").dialog().parents(".ui-dialog").find(".ui-dialog-titlebar").remove();
 				},
 				width : 400,
 				height : 600,
 				resizable : false,
-				draggable : true
+				draggable : true,
+				buttons: { "close": function() { $(this).dialog("close"); } , "next" : function() {if(curIndex == url_category_text.length - 1){curIndex = -1;}$(this).load(url_category_text[++curIndex][0]); doWhenDialogLoad(this, url_category_text[curIndex][1], url_category_text[curIndex][2]); } 
+				, "previous" : function() {if(curIndex == 0){curIndex = url_category_text.length;}$(this).load(url_category_text[--curIndex][0]); doWhenDialogLoad(this, url_category_text[curIndex][1], url_category_text[curIndex][2]); }} 
 			});
 }
 
